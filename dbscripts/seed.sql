9c186ea38b314b72d65702630b4324b95181b92d
Ben Dickman <Ben.Dickman@connellsgroup.co.uk>
2023-06-08 17:12:04 +0100

    update the seed data

--- dbscripts/seed.sql
+++ dbscripts/seed.sql
@@ -10,7 +10,13 @@ CREATE TABLE blog
 
 ALTER TABLE "blog" OWNER TO bloguser;
 
-Insert into blog(title,description) values( 'Title 1','Description 1');
-Insert into blog(title,description) values( 'Title 2','Description 2');
-Insert into blog(title,description) values( 'Title 3','Description 3');
-Insert into blog(title,description) values( 'Title 4','Description 4');
+Insert into blog(title,description) values( 'Ben Test Title 1','Ben Test Description');
+Insert into blog(title,description) values( 'Ben Test Title 2','Ben Test Description');
+Insert into blog(title,description) values( 'Ben Test Title 3','Ben Test Description');
+Insert into blog(title,description) values( 'Ben Test Title 4','Ben Test Description');
+Insert into blog(title,description) values( 'Ben Test Title 5','Ben Test Description');
+Insert into blog(title,description) values( 'Ben Test Title 6','Ben Test Description');
+Insert into blog(title,description) values( 'Ben Test Title 7','Ben Test Description');
+Insert into blog(title,description) values( 'Ben Test Title 8','Ben Test Description');
+Insert into blog(title,description) values( 'Ben Test Title 9','Ben Test Description');
+Insert into blog(title,description) values( 'Ben Test Title 10','Ben Test Description');
